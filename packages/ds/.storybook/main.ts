6e747b2ddcf7779fbeca1d2dd6fec47b78a75d42
Jorge Machado <jorge.vmachado@hotmail.com>
2025-01-22 15:50:11 -0300

    fix storybook ds error

--- packages/ds/.storybook/main.ts
+++ packages/ds/.storybook/main.ts
@@ -4,30 +4,21 @@ import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 
 import sass from 'sass';
 
-import { join, dirname } from 'path';
-
 const currentBrand = process.env.BRAND || 'geek';
 
 const brand = currentBrand.replace(/\s/g, '');
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-webpack5-compiler-swc'),
-    getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/addon-interactions'),
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: getAbsolutePath('@storybook/react-webpack5'),
+    name: '@storybook/react-webpack5',
     options: {},
   },
   webpackFinal: async (config) => {
