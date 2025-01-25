a41b9d67c9fd0cc4002e6f89789cfe7da5962758
Jorge Machado <jorge.vmachado@hotmail.com>
2025-01-24 21:36:38 -0300

    Feito o code review do PageContent component no UI layouts

--- packages/ui/src/layout/page-content/PageContent.tsx
+++ packages/ui/src/layout/page-content/PageContent.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
 
+import joinClass from '@repo/ds/utils/join-class/joinClass';
+
 import Text from '@repo/ds/elements/text/Text';
 
 import './PageContent.scss';
 
-interface PageContentProps {
+interface PageContentProps extends React.HTMLAttributes<HTMLDivElement> {
   title?: string;
   children: React.ReactNode;
+  className?: string;
+  ariaLabelledBy?: string;
+  contentClassName?: string;
 }
 
-export default function PageContent({ title, children }: PageContentProps) {
+export default function PageContent({
+  title,
+  children,
+  className = '',
+  ariaLabelledBy,
+  contentClassName = '',
+  ...props
+}: PageContentProps) {
   return (
-    <div className="page-content">
+    <div
+      className={joinClass(['page-content', className])}
+      aria-labelledby={ariaLabelledBy}
+      {...props}
+    >
       {title && (
         <Text tag="h1" weight="bold" variant="xlarge">
           {title}
         </Text>
       )}
-      <div>{children}</div>
+      <div className={contentClassName}>{children}</div>
     </div>
   );
 }
