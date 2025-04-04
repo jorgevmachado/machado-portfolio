4eed75c2b3b0da83c42bbdd0be84f2b404cbdeac
jorge.machado-Kraken <jorge.vmachado@hotmail.com>
2025-04-04 13:13:56 -0300

    Add comprehensive tests for service methods
    Expanded service test coverage to include softRemove, findAll, findOne, remove, treatEntityParam(s), and findOneByList methods. These tests ensure the reliability of various entity handling operations, improving overall code quality and robustness.

--- apps/api/src/shared/service/service.spec.ts
+++ apps/api/src/shared/service/service.spec.ts
@@ -51,6 +51,17 @@ class TestService extends Service<MockEntity> {
 }
 
 describe('service', () => {
+  const entity = {
+    id: '52422c37-83a7-4ca6-96c4-bd15a1dc61bc',
+    name: 'test',
+  };
+  const entities = [
+    entity,
+    {
+      id: '92ca29f3-d08d-4abe-a157-94ef70f512c7',
+      name: 'test2',
+    },
+  ];
   let service: TestService;
 
   beforeEach(() => {
@@ -98,17 +109,100 @@ describe('service', () => {
 
   describe('save', () => {
     it('should save entity successfully', async () => {
-      const entity = {
-        id: '1',
-        name: 'test',
-      };
-
       jest.spyOn(mockRepository, 'save').mockResolvedValueOnce(entity);
 
       const result = await service.save(entity);
 
       expect(mockRepository.save).toBeCalledTimes(1);
       expect(mockRepository.save).toBeCalledWith(entity);
+      expect(result).toEqual(entity);
+    });
+  });
+
+  describe('softRemove', () => {
+    it('should softRemove entity successfully', async () => {
+      jest.spyOn(mockRepository, 'softRemove').mockResolvedValueOnce(entity);
+
+      const result = await service.softRemove(entity);
+
+      expect(mockRepository.softRemove).toBeCalledTimes(1);
+      expect(mockRepository.softRemove).toBeCalledWith(entity);
+      expect(result).toEqual(entity);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an list of entities', async () => {
+      jest.spyOn(Queries.prototype, 'list').mockResolvedValueOnce(entities);
+      const result = await service.findAll({});
+      expect(result).toEqual(entities);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return one entity', async () => {
+      jest.spyOn(Queries.prototype, 'findOne').mockResolvedValueOnce(entity);
+      const result = await service.findOne({ value: entity.id });
+      expect(result).toEqual(entity);
+    });
+  });
+
+  describe('remove', () => {
+    it('should return one entity', async () => {
+      jest.spyOn(Queries.prototype, 'findOne').mockResolvedValueOnce(entity);
+      jest.spyOn(mockRepository, 'softRemove').mockResolvedValueOnce(entity);
+      const result = await service.remove(entity.id);
+      expect(result).toEqual({ message: 'Successfully removed' });
+    });
+  });
+
+  describe('treatEntityParam', () => {
+    it('should return one entity by request', async () => {
+      jest.spyOn(Queries.prototype, 'findOne').mockResolvedValueOnce(entity);
+      const result = await service.treatEntityParam(entity.id, 'Entity');
+      expect(result).toEqual(entity);
+    });
+
+    it('should return one entity by list and name', async () => {
+      const result = await service.treatEntityParam(
+        entity.name,
+        'Entity',
+        entities,
+      );
+      expect(result).toEqual(entity);
+    });
+
+    it('should return one entity when the parameter is a entity', async () => {
+      jest.spyOn(Validate.prototype, 'paramIsEntity').mockReturnThis();
+      const result = await service.treatEntityParam(entity, 'Entity');
+      expect(result).toEqual(entity);
+    });
+  });
+
+  describe('treatEntitiesParams', () => {
+    it('should return an list of entities empty', async () => {
+      const result = await service.treatEntitiesParams();
+      expect(result).toEqual([]);
+    });
+
+    it('should return an list of entities empty with list of entities empty', async () => {
+      const result = await service.treatEntitiesParams([]);
+      expect(result).toEqual([]);
+    });
+    it('should return an list of entities by request', async () => {
+      jest.spyOn(Queries.prototype, 'list').mockResolvedValueOnce(entities);
+      const result = await service.treatEntitiesParams(entities, 'Entity');
+      expect(result).toEqual(entities);
+    });
+  });
+
+  describe('findOneByList', () => {
+    it('should return one entity by list and name', () => {
+      const result = service.findOneByList(entity.name, entities);
+      expect(result).toEqual(entity);
+    });
+    it('should return one entity by list and uuid', () => {
+      const result = service.findOneByList(entity.id, entities);
       expect(result).toEqual(entity);
     });
   });
