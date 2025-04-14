d2257ac7da1197fefd3dc16b84037698789d36a5
jorge.machado-Kraken <jorge.vmachado@hotmail.com>
2025-04-14 19:03:57 -0300

    Simplify Dashboard component by removing modal logic.
    The modal functionality, including related state and components, has been removed from the Dashboard. This change reduces component complexity and focuses the Dashboard solely on rendering its primary content.

--- apps/finance/src/pages/dashboard/Dashboard.tsx
+++ apps/finance/src/pages/dashboard/Dashboard.tsx
@@ -1,56 +1,9 @@
-import React, { useState } from 'react';
-
-import Button from '@repo/ds/components/button/Button';
-
-import useModal from '@repo/ds/components/modal/useModal';
-
-interface BodyModalProps {
-  count: number;
-  onClose: () => void;
-  onAgree: () => void;
-}
-function BodyModal({ count, onClose, onAgree }: BodyModalProps) {
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <div style={{ display: 'grid', gap: 15, gridTemplateColumns: '1fr 1fr' }}>
-        <Button fluid appearance="outline" onClick={onClose}>
-          Fechar
-        </Button>
-        <Button fluid onClick={onAgree}>
-          Incrementar
-        </Button>
-      </div>
-    </div>
-  );
-}
+import React from 'react';
 
 export default function Dashboard() {
-  const { openModal, modal } = useModal();
-  const [count, setCount] = useState<number>(0);
-
-  const handleOpenModal = () => {
-    openModal({
-      title: `Título Modal ${count}`,
-      body: (
-        <BodyModal
-          count={count}
-          onClose={() => console.log('Modal fechada')}
-          onAgree={() => {
-            setCount(count + 1);
-            console.log('Ação de salvar definida!');
-          }}
-        />
-      ),
-      closeOnEsc: true,
-      closeOnOutsideClick: true,
-    });
-  };
-
   return (
     <div>
-      <Button onClick={handleOpenModal}>Abrir Modal</Button>
-      {modal}
+      <h1>Dashboard</h1>
     </div>
   );
 }
